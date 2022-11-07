853a138397442a9c0c7f47b7aec0e046589b8fda
Moawiah999 <muawiyah454@gmail.com>
2022-11-07 13:58:32 +0200

    addseconfscreen

---
+++ lib/secondscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SeconeScreen extends StatelessWidget {
+  const SeconeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text("12345")),
+    );
+  }
+}
